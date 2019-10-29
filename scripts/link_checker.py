fe7b48f168d716a5c85cef73562b6a1b32231f17
Sébastien Lavoie <sgdlavoie@gmail.com>
2019-10-29 14:04:25 -0600

    Add `scripts` directory
    - The scripts in this directory are used to automate specific tasks
    within this repository. There might be some more things to do later, of
    course...

---
+++ scripts/link_checker.py
@@ -0,0 +1,149 @@
+"""Simple link checker that will recursively extract URLs from .md files
+from the current repository.
+
+Asynchronous features with the help of the following page:
+https://stackoverflow.com/a/13530258/8787680
+"""
+import multiprocessing as mp
+import os
+import subprocess
+import time
+
+# Third-party imports
+import requests
+
+
+FILE_NAME = "dead.txt"  # Will appear if it contains dead links
+HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
+        "(KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36"
+    )
+}
+
+EXCLUDED_WEBSITES = [
+    "calendar.google.com",
+    "challengerocket.com",
+    "https://yukaichou.com/gamification-book/",
+]
+
+
+def extract_links(excluded_websites: list) -> list:
+    """Extract links from all .md files recursively from the current
+    repository."""
+    cmd = (
+        'cat ../**/*.md | grep -Eo "(http|https)://[a-zA'
+        '-Z0-9./?=_~-]*" | sort | uniq > links.txt'
+    )
+    subprocess.run(cmd, shell=True)
+    print("Links extracted from repository.")
+
+    sites = []
+    with open("links.txt") as f:
+        content = f.readlines()
+        for line in content:
+            excluded = False
+            for website in excluded_websites:
+                if website in line:
+                    print(f"Excluding: {website}")
+                    excluded = True
+            if not excluded:
+                line = line.strip()
+                sites.append(line)
+
+    os.unlink("links.txt")
+    return sites
+
+
+def download_site(url, q):
+    """Request an URL. If it can properly be retrieved, add it to queue
+    `q` so that it gets written to a file with the job manager."""
+    global HEADERS
+    try:
+        with requests.get(url, headers=HEADERS) as response:
+            response_length = len(response.content)
+            print(f"Current URL: {url}")
+            if not response_length or not response.ok:
+                if response.status_code != 403:  # ignore blocked requests
+                    q.put(f"[{response.status_code}] {url}")
+    except:  # Any kind of error occurring
+        try:
+            q.put(f"[{response.status_code}] {url}")
+        except UnboundLocalError:  # response doesn't exist → connection error
+            q.put(url)
+
+
+def check_all_ok(dead_file):
+    """Once we have requested all URLs, check if any was found to be
+    dead and print them with their specific error code."""
+    try:
+        with open(dead_file) as f:
+            content = f.readlines()
+            if not content:  # We get an empty list
+                print("No dead link found.")
+                os.unlink(dead_file)
+            else:
+                print("Dead links found:\n")
+                for line in content:
+                    print(line.strip())
+    except FileNotFoundError:
+        print(f"<{dead_file}> doesn't exist.")
+
+
+def listener(q):
+    """Listens for messages on the `q`, writes to file."""
+
+    with open(FILE_NAME, "w") as f:
+        while True:
+            m = q.get()
+
+            # String received, signaling we're done
+            if m == "kill":
+                break
+
+            f.write(str(m) + "\n")
+            f.flush()
+
+
+def job_manager(sites):
+    """The purpose of the job manager is to add "jobs" in a "queue" so
+    that at any time only one "job" will be written to a file thanks to
+    a "listener" that receives incoming "jobs" from the "queue"."""
+    # Must use Manager queue here, or will not work
+    manager = mp.Manager()
+    q = manager.Queue()
+    pool = mp.Pool(mp.cpu_count() + 2)
+
+    # Put listener to work first
+    _ = pool.apply_async(listener, (q,))
+
+    # Fire off workers
+    jobs = []
+    for site in sites:
+        job = pool.apply_async(download_site, (site, q))
+        jobs.append(job)
+
+    # collect results from the workers through the pool result queue
+    for job in jobs:
+        job.get()
+
+    # Now we are done, kill the listener
+    q.put("kill")
+    pool.close()
+
+
+if __name__ == "__main__":
+    SITES = extract_links(EXCLUDED_WEBSITES)  # Generate list of links
+    START_TIME = time.time()  # Start timer right before launching job manager
+
+    # Add links as 'jobs' to write dead links to a file
+    job_manager(SITES)
+
+    DURATION = time.time() - START_TIME
+    print(f"Checked {len(SITES)} in {DURATION:.4f} seconds")
+
+    # Scan through the file that contains dead links. If empty, delete it.
+    check_all_ok(FILE_NAME)
+
+    # Play a nice little sound when the script is done
+    subprocess.run("aplay /home/sglavoie/Music/.level_up.wav", shell=True)
---
+++ scripts/pdf-compress.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+## Script to compress PDF Files using ghostscript incl. subdirs
+## Copyright (C) 2016 Maximilian Fries - All Rights Reserved
+## Contact: maxfries@t-online.de
+## Last revised 2016-07-29
+
+# Usage
+# ./pdf-compress.sh [screen|ebook|prepress|default] [verbose]
+
+# Variables and preparation
+{
+count=0
+success=0
+successlog=./success.tmp
+gain=0
+gainlog=./gain.tmp
+pdfs=$(find ../ -type f -name "*.pdf")
+total=$(echo "$pdfs" | wc -l)
+log=./log
+verbose="-dQUIET"
+mode="prepress"
+echo "0" | tee $successlog $gainlog > /dev/null
+}
+
+# Are there any PDFs?
+if [ "$total" -gt 0 ]; then
+
+#Parameter Handling & Logging
+{
+  echo "-- Debugging for Log START --"
+  echo "Number of Parameters: $#"
+  echo "Parameters are: $*"
+  echo "-- Debugging for Log END   --"
+} >> $log
+
+# Only compression-mode set
+if [ $# -eq 1 ]; then
+  mode="$1"
+fi
+
+# Also Verbose Level Set
+if [ $# -eq 2 ]; then
+  mode="$1"
+  verbose=""
+fi
+
+echo "$pdfs" | while read -r file
+do
+  ((count++))
+  echo "Processing File #$count of $total Files" | tee -a $log
+  echo "Current File: $file "| tee -a $log
+  gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS="/$mode" -dNOPAUSE \
+  -dBATCH $verbose -sOutputFile="$file-new" "$file" | tee -a $log
+
+  sizeold=$(wc -c "$file" | cut -d' ' -f1)
+  sizenew=$(wc -c "$file-new" | cut -d' ' -f1)
+  difference=$((sizenew-sizeold))
+
+  # Check if new filesize is smaller
+  if [ $difference -lt 0 ]
+  then
+    rm "$file"
+    mv "$file-new" "$file"
+    printf "Compression was successfull. New File is %'.f Bytes smaller\n" \
+    $((-difference)) | tee -a $log
+    ((success++)) 
+    echo $success > $successlog
+    ((gain-=difference))
+    echo $gain > $gainlog
+  else
+    rm "$file-new"
+    echo "Compression was not necessary" | tee -a $log
+  fi
+
+done
+
+# Print Statistics
+printf "Successfully compressed %'.f of %'.f files\n" $(cat $successlog) $total | tee -a $log
+printf "Safed a total of %'.f Bytes\n" $(cat $gainlog) | tee -a $log
+
+rm $successlog $gainlog
+
+else
+  echo "No PDF File in Directory"
+fi
